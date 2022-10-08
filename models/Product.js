4626bc3df6c4bf9c275bd83110a02c8a7b6d5b7a
Jeff Small <jeffmeister1990@gmail.com>
2022-10-08 17:01:59 -0400

    Finished Product.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,48 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+
+    },
+
+    // product name column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    // price column
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: { isDecimal: true }
+    },
+
+    // stock column
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    // category id column
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
